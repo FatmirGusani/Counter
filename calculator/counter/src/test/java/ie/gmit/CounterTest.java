ab3e4bcfd57e402406fb49b33dc7878b2e7ceadd
Fatmir Gusani <G00338685@gmit.ie>
2020-02-10 10:44:32 +0000

    Testing Junit and Maven

---
+++ calculator/counter/src/test/java/ie/gmit/CounterTest.java
@@ -0,0 +1,18 @@
+package ie.gmit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+
+public class CounterTest {
+
+    Counter myCounter;
+
+    @Test
+    void TestConstructor()
+    {
+        myCounter = new Counter();
+        assertEquals(0,myCounter.getCount());
+    }
+}
